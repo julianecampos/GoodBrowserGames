541a07ed7d3d5549146e9336eb08ba21680e6ed7
Juliane Campos <64619650+julianecampos@users.noreply.github.com>
2022-04-02 20:32:20 -0300

    Add files via upload

---
+++ GoodBrowserGames/src/test/java/com/goodBrowserGames/GoodBrowserGames/GoodBrowserGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodBrowserGames.GoodBrowserGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodBrowserGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
